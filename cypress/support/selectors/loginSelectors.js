547ce1f8850d063af8a34477f6ac160a9a8d87fb
mkyPops <gamingmky5@gmail.com>
2025-12-24 17:10:59 +0500

    Login pages selecrors

---
+++ cypress/support/selectors/loginSelectors.js
@@ -0,0 +1,8 @@
+export const loginSelectors={
+    UserName: '[name="username"]',
+    Password: '[placeholder="Password"]',
+    LogIn: '.oxd-button.oxd-button--medium',
+    ForgotPassowrd: 'oxd-text oxd-text--p orangehrm-login-forgot-header',
+    InavlidCredentialsError: '.oxd-alert-content.oxd-alert-content--error',
+    logo: '.orangehrm-login-logo',
+}
