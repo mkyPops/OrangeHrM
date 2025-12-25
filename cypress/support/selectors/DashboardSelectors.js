1c8952f3b71d7321cd0f252374f3cc92f81c2ca5
mkyPops <gamingmky5@gmail.com>
2025-12-25 16:30:29 +0500

    Dashboard selectors

---
+++ cypress/support/selectors/DashboardSelectors.js
@@ -0,0 +1,12 @@
+export const DashboardSelectors ={
+    HeaderTitle:'.oxd-topbar-header-title',
+    DropDown:'.oxd-icon.bi-caret-down-fill.oxd-userdropdown-icon',
+    UserName:'.oxd-userdropdown-name',
+    // it has 4 option us eq(0) for about, (1) for support, (2) Change Password, (3) Logout
+    LinksOfDropDown:'.oxd-userdropdown-link', 
+    SideBarIcon:'.oxd-icon-button.oxd-main-menu-button',
+      
+
+
+
+}
