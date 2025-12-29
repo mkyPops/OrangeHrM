648fee25ba66d333c2b9129e9f6acbf03675ffc4
mkyPops <gamingmky5@gmail.com>
2025-12-29 15:22:30 +0500

    DashboardSelectors compeleted till side panels

--- cypress/support/selectors/DashboardSelectors.js
+++ cypress/support/selectors/DashboardSelectors.js
@@ -1,12 +1,11 @@
 export const DashboardSelectors ={
     HeaderTitle:'.oxd-topbar-header-title',
     DropDown:'.oxd-icon.bi-caret-down-fill.oxd-userdropdown-icon',
-    UserName:'.oxd-userdropdown-name',
-    // it has 4 option us eq(0) for about, (1) for support, (2) Change Password, (3) Logout
     LinksOfDropDown:'.oxd-userdropdown-link', 
     SideBarIcon:'.oxd-icon-button.oxd-main-menu-button',
-      
-
-
-
+    LogoutLink:'a[href="/web/index.php/auth/logout"]',
+    ChangePassword:'a[href="/web/index.php/pim/updatePassword"]',
+    SupportLink:'a[href="/web/index.php/help/support"]',
+    AboutLink:'a[href="#"]',
+    ToDoList:'.orangehrm-todo-list'
 }
