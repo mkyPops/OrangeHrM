434b476474e32547da3a9245a2092a1cd02d41b3
mkyPops <gamingmky5@gmail.com>
2026-01-06 17:31:50 +0500

    Selectors part 2

--- cypress/support/selectors/AdminSelectors.js
+++ cypress/support/selectors/AdminSelectors.js
@@ -1,3 +1,8 @@
 export const AdminSelectors={
-    admin
+    AccessingAdmin:'.oxd-main-menu-item.active',
+    SearchPanel:'[placeholder="Search"]',
+    UserName:'input.oxd-input.oxd-input--active',     // make sure to use eq(1) with it 
+    
+
+    
 }
